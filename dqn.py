5c658df1d683a6f5358673705f0d3aff46f11565
anko0116 <hyukahn@umich.edu>
2020-01-16 00:21:42 -0500

    Notes of DQN architecture

--- dqn.py
+++ dqn.py
@@ -1,11 +1,4 @@
 '''
-We will first try DQN with CNN architecture mentioned in the
-Atari game research paper
-'''
-
-import tensorflow as tf
-
-'''
 Input state: current hoist position(s), hoist current weight capacity,
 and call request (up/down) and its index of floor
 
@@ -16,21 +9,38 @@ and call request (up/down) and its index of floor
 
 - Total: (#_floors x #_elevators + 1 + 1 + 1)
 
+---------------------------------------
 
+TURN SIMULATION STATE INTO AN IMAGE (LIKE ATARI GAME)
 height = #_floors
-width = (DOWN calls = 1) + (UP calls = 1) + (#_elevators = 1) 
-depth = (DOWN, UP calls = 0 or 1) or (Each elevator's capacity normalized)
+width = (DOWN calls = 1) + (UP calls = 1) + (#_elevators = 2 for now) 
+depth = 
+    - DOWN, UP calls = 0 or 1
+    - Each elevator's capacity normalized = [0, 1]
 ---------------------
-|P |                |
-|A |                |
-|S |                | 
-|S |                |
-|  |                |
-|  |                |
-|  |                |
+|D |   | E |        |
+|O |   | L |        |
+|W |   | E |        | 
+|N |   |   |        |
+|  |   |   | E |    |
+|  | U |   | L |    |
+|  | P |   | E |    |
 ---------------------
+
+First conv layer: kernel size = 3x3
+
+Cautions:
+- Will CNN work fine on a narrow image?
+
+Deep reinforcmenet learning on changing data sizes
+https://datascience.stackexchange.com/questions/61536/deep-reinforcement-learning-on-changing-data-sizes
 '''
 
+import tensorflow as tf
+
 class DQN():
+    def create_model():
+        
+
     def __init__():
 
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import simpy
-
+import numpy as np
+import torch
+'''
 def pass_gen(env):
     while True:
         yield env.timeout(500)
@@ -9,4 +11,10 @@ env = simpy.Environment()
 env.process(pass_gen(env))
 env.run(until=env)
 print("Finished test")
+'''
+class DQN(torch.nn.Module):
+    def __init__(self):
+        super(DQN, self).__init__()
+
+        self.conv1 = torch.nn.Conv2d()
 
