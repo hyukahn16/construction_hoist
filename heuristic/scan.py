a8a8b8c99ffc2f5ce52304fd1ec1f8d88383c60c
anko0116 <hyukahn@umich.edu>
2020-04-09 17:59:32 -0400

    Start elevator heuristic algorithm

---
+++ heuristic/scan.py
@@ -0,0 +1,5 @@
+'''
+Heuristic algorithm for Elevator which follows the SCAN algorithm
+1. proceed in the same direction until the last request in that direction.
+2. If there is no request, stop and proceed towards other direction if there is any requests from the other direction
+Source: https://www.quora.com/What-algorithm-is-used-in-modern-day-elevators
