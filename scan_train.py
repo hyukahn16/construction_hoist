979d5c790e5edf86a89452c2a60756038a7c0d5e
anko0116 <hyukahn@umich.edu>
2020-04-14 16:03:00 -0400

    Implement correct training loop for the heuristic

--- scan_train.py
+++ scan_train.py
@@ -1,11 +1,11 @@
 import environment as gym
 import time
-import numpy as numpy
+import numpy as np
 from heuristic import *
 import copy
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 
 def organize_output(output, new_output):
     for e_id, e_output in output.items():
@@ -20,18 +20,9 @@ total_floors = 10
 pass_gen_time = 75
 
 nS = total_floors * 4
-nA = 3
-lr = 0.001
-gamma = 0.95
-eps = 1
-min_eps = 0.01
-eps_decay = 0.99999
-batch_size = 24
-
-use_saved = True
 
 neg_action = [-1 for i in range(num_elevators)] # Used for state's next actions
-agents = [ScanAgent() for _ in range(num_elevators)]
+agents = [ScanAgent(total_floors) for _ in range(num_elevators)]
 env = gym.make(num_elevators, total_floors, total_floors, pass_gen_time)
 
 episode_rewards = []
@@ -39,7 +30,7 @@ for e in range(10000): # number of episodes
     print("-----------{} Episode------------".format(e))
     output = {}
     organize_output(output, env.reset())
-    
+
     cumul_rewards = [0 for _ in range(num_elevators)]
     cumul_actions = {0: [0,0,0]}
     while env.now() <= 1000: # Force-stop episode if time is over
@@ -57,27 +48,15 @@ for e in range(10000): # number of episodes
         # 2. Take action in the Environment
         new_output = env.step(actions)
 
-        # 3. Update Replay Memory and train agent
+        # 3. Update reward
         for e_id, e_output in new_output.items():
             cumul_rewards[e_id] += e_output["reward"]
-            replay_action = actions[e_id]
-
-            state = copy.deepcopy(output[e_id]["state"])
-            new_state = copy.deepcopy(e_output["state"])
-            state = [state]
-            new_state = [new_state]
-
-            agents[e_id].store(state, replay_action, e_output["reward"], 
-                                new_state, 1.0)
-            if len(agents[e_id].memory) > batch_size: 
-                agents[e_id].experience_replay(batch_size)
-
+            
         # 4. overwrite old output with new output
         organize_output(output, new_output) # FIXME: need to distinguishi which elevator was the decision elevator last time
 
     # Outside of episode
     print("Rewards: ", cumul_rewards)
-    print("Epsilon value:", agents[0].epsilon)
     print("Elevator_1 Number of passengers served: ", env.elevators[0].num_served)
     print("Elevator_1 Number of passengers carrying: ", len(env.elevators[0].passengers))
     print("Elevator_1 Max floor visited: ", env.elevators[0].max_visited)
@@ -86,7 +65,7 @@ for e in range(10000): # number of episodes
     print("Total passengers generated:", env.generated_passengers)
     episode_rewards.append(cumul_rewards[0])
     plt.plot([i for i in range(e + 1)], episode_rewards)
-    plt.pause(0.01)
+    plt.pause(0.8)
     plt.draw()
 
     # Save model
