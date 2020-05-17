dc42b4de7fb7845fe033a6fc79c6e1aa49d7a745
anko0116 <hyukahn@umich.edu>
2020-05-17 17:45:01 -0400

    Start HumanAgent heuristic algorithm

---
+++ heuristic/human.py
@@ -0,0 +1,18 @@
+'''
+Heuristic algorithm for Elevator which follows basic human-like control
+1. Maintain a queue of service requests from passengers
+2. Deliver passengers to their destination based on the queue
+'''
+
+import queue
+
+class HumanAgent():
+    def __init__(self):
+        self.passenger_queue = queue.Queue()
+
+    def action(self, state):
+        
+
+
+
+    
