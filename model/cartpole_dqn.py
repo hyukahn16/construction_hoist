e9add1ad8062fbfe7704993d39815a7df7615f7f
anko0116 <hyukahn@umich.edu>
2020-05-21 18:13:38 -0400

    Change save frequency to 5 (lower seems to be better)

--- model/cartpole_dqn.py
+++ model/cartpole_dqn.py
@@ -28,7 +28,7 @@ class DeepQNetwork():
         checkpoint_path = 'training_1/cp.ckpt'
         self.checkpoint_dir = os.path.dirname(checkpoint_path)
         self.cp_callback = keras.callbacks.ModelCheckpoint(filepath=self.checkpoint_dir,
-                                                            save_freq=batch_size,
+                                                            save_freq=5,
                                                             save_weights_only=True,
                                                             verbose=0)
 
