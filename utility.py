4f2bab99f9e0e61aea90adf63ea45bebfa023a03
anko0116 <hyukahn@umich.edu>
2020-06-16 07:10:58 -0400

    Create file to house all the utility functions

---
+++ utility.py
@@ -0,0 +1,7 @@
+def organize_output(output, new_output):
+    for e_id, e_output in output.items():
+        e_output["last"] = False
+
+    for e_id, e_output in new_output.items():
+        output[e_id] = e_output
+        output[e_id]["last"] = True
