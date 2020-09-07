143bde806aa9c07d607f365855624d833414f10b
anko0116 <hyukahn@umich.edu>
2020-09-07 21:48:06 +0900

    Add hyperparameters for PPO

--- hyperparam.py
+++ hyperparam.py
@@ -1,19 +1,29 @@
 # Environment variables
 num_elevators = 1
 total_floors = 20
-pass_gen_time = 80
+pass_gen_time = 160
 episode_time = 5000
 num_episodes = 10000
 
-# DQN variables
+# general network variables
 nS = total_floors * 4
 nA = 3
 lr = 0.001
 gamma = 0.95
+batch_size = 24
+
+# DQN exclusive variables
 eps = 1
 min_eps = 0.1
 eps_decay = 0.99999
-batch_size = 24
+
+# PPO exclusive variables
+ppo_epochs = 8
+ppo_steps = 256
+critic_discount = 0.5
+entropy_beta = 0.001
+clip_range = 0.2
+lmbda = 0.95
 
 # Testing variables
 use_saved = False
